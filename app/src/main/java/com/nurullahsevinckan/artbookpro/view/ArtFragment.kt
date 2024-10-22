ed5cc67cfb04a1711fc2eba8afb4b8726dc523d5
SNurullahS <nurullah.sevinckan91@gmail.com>
2024-10-23 00:43:49 +0300

    View binding for artFragment added

--- app/src/main/java/com/nurullahsevinckan/artbookpro/view/ArtFragment.kt
+++ app/src/main/java/com/nurullahsevinckan/artbookpro/view/ArtFragment.kt
@@ -3,11 +3,21 @@ package com.nurullahsevinckan.artbookpro.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.nurullahsevinckan.artbookpro.R
+import com.nurullahsevinckan.artbookpro.databinding.FragmentArtsBinding
 
 class ArtFragment : Fragment(R.layout.fragment_arts) {
 
+    private var fragmentBinding : FragmentArtsBinding? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentArtsBinding.bind(view)
+        fragmentBinding = binding
+
+        binding.floatingActionButton.setOnClickListener{
+            findNavController().navigate(ArtFragmentDirections.actionArtFragmentToArtDetailsFragment())
+        }
     }
 }
